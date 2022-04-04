c365e2c6a468b2eb4d5dcad5e1dad96fc7cebdcc
UmamageshwariSubramani <102666653+UmamageshwariSubramani@users.noreply.github.com>
2022-04-05 04:17:30 +0530

    Add files via upload

---
+++ Desktop.java
@@ -0,0 +1,20 @@
+package org.system;
+
+public class Desktop extends Computer {
+	
+	public void desktopSize() {
+		System.out.println("DesktopSize");
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Desktop model=new Desktop();
+		model.computerModel();
+		model.desktopSize();
+		
+
+	}
+
+}
